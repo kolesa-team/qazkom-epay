d3b683173310b77c25c6483be47f743a8fcc6609
Igor Borodikhin <iborodikhin@gmail.com>
2015-12-08 12:10:36 +0600

    Добавлен метод для создания подтверждения

--- src/Client.php
+++ src/Client.php
@@ -170,12 +170,92 @@ class Client
         );
 
         $request = $this->processXml($this->config['XML_TEMPLATE_FN'], $request);
-        $sign = new Sign($this->config);
+        $sign    = new Sign($this->config);
         $sign->setInvert(true);
 
         $xml = sprintf(
-            '<document>%s<merchant_sign type="RSA">%s</merchant_sign></document>',
+            '<document>%s<merchant_sign type="RSA" cert_id="%s">%s</merchant_sign></document>',
             $request,
+            $this->config['MERCHANT_CERTIFICATE_ID'],
+            $sign->sign64($request)
+        );
+
+        if ($base64encode) {
+            $xml = base64_encode($xml);
+        }
+
+        return $xml;
+    }
+
+    /**
+     * Создаёт подписанное XML-подтверждение.
+     *
+     * @param  string                   $reference
+     * @param  string                   $approvalCode
+     * @param  integer                  $orderId
+     * @param  integer                  $currencyCode
+     * @param  integer                  $amount
+     * @param  boolean                  $base64encode
+     * @return string
+     * @throws Amount\EmptyAmount
+     * @throws Certificate\UnknownError
+     * @throws Order\EmptyId
+     * @throws Order\NotNumeric
+     * @throws Order\NullId
+     */
+    public function processConfirmation(
+        $reference,
+        $approvalCode,
+        $orderId,
+        $currencyCode,
+        $amount,
+        $base64encode = true
+    ) {
+        switch (true) {
+            case strlen($orderId) < 1:
+                throw new Order\EmptyId();
+                break;
+
+            case !is_numeric($orderId):
+                throw new Order\NotNumeric();
+                break;
+
+            case $orderId < 1:
+                throw new Order\NullId();
+                break;
+
+            case $amount == 0:
+                throw new Amount\EmptyAmount();
+                break;
+
+            case strlen($this->config['PRIVATE_KEY_FN']) == 0:
+                throw new Certificate\UnknownError('Path for Private key not found');
+                break;
+
+            case strlen($this->config['XML_TEMPLATE_FN']) == 0:
+                throw new Certificate\UnknownError('Path for Private key not found');
+                break;
+        }
+
+        $request = array(
+            'MERCHANT_CERTIFICATE_ID' => $this->config['MERCHANT_CERTIFICATE_ID'],
+            'MERCHANT_NAME'           => $this->config['MERCHANT_NAME'],
+            'ORDER_ID'                => $orderId,
+            'CURRENCY'                => $currencyCode,
+            'REFERENCE'               => $reference,
+            'APPROVAL_CODE'           => $approvalCode,
+            'MERCHANT_ID'             => $this->config['MERCHANT_ID'],
+            'AMOUNT'                  => $amount,
+        );
+
+        $request = $this->processXml($this->config['XML_TEMPLATE_CONFIRM_FN'], $request);
+        $sign    = new Sign($this->config);
+        $sign->setInvert(true);
+
+        $xml = sprintf(
+            '<document>%s<merchant_sign type="RSA" cert_id="%s">%s</merchant_sign></document>',
+            $request,
+            $this->config['MERCHANT_CERTIFICATE_ID'],
             $sign->sign64($request)
         );
 
